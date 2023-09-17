b9214a381f33bee006248d5b0ba18db3cc93637c
milo2112 <mjrp.ros@gmail.com>
2023-09-17 17:35:52 -0300

    refactoring "operaciones.js" and "index.js"  files

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const operaciones = require('./utils/operaciones')
 const [operacion, nombre, edad, animal, color, enfermedad] = process.argv.slice(2)
 
-// console.log(`contenido --> ${operacion}, ${nombre}, ${edad}, ${animal}, ${color}, ${enfermedad}`)
-
 if (operacion === 'registrar') {
   operaciones.registrar(nombre, edad, animal, color, enfermedad)
 }
